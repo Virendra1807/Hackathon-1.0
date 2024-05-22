fad9f40254fc5fbc0d7a87728a152f852ab64a78
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-23 03:15:22 +0530

    Attended manual validation for form

---
+++ Survey-Form/survey-form.js
@@ -0,0 +1,11 @@
+
+
+function submitFun(){
+    let txt = document.getElementById("name");
+    let text = txt.value;
+    if( text == ""){
+        alert('Enter the mandatory details' );
+    }else{
+        alert('Hey '+ text +' Your form is submitted Successfully ! ');
+    }
+}
